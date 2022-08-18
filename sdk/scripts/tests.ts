8bf021ef17ae48c6454a885862c87e805495b246
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2022-08-18 17:08:24 +0200

    Test stake

--- sdk/scripts/tests.ts
+++ sdk/scripts/tests.ts
@@ -1,5 +1,17 @@
+import {AptosClient, FaucetClient} from "aptos";
+import {loadAdminFromConfig, TESTNET_URL, TestWallet} from "../src/utils";
+import {FAUCET_URL} from "aptos/dist/util.test";
+import {Staker} from "../src/staker";
+
 const main = async () => {
-    console.log('hello world!')
+    const aptosClient = new AptosClient(TESTNET_URL)
+    const faucetClient = new FaucetClient(TESTNET_URL, FAUCET_URL)
+    const admin = await loadAdminFromConfig()
+    const contractAddress = admin.toPrivateKeyObject().address as string
+    const wallet = new TestWallet(admin)
+    const staker = await Staker.build({ aptosClient, faucetClient, wallet, contractAddress })
+    let tx = await staker.stake(10);
+    console.log(tx)
 }
 
 main()
