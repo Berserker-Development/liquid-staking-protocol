e157c2f8ed16ad49560c82dd7e30c94b21832af7
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2022-08-29 18:54:45 +0200

    Add add-validator script

---
+++ sdk/scripts/add-validator.ts
@@ -0,0 +1,16 @@
+import {AptosClient, FaucetClient} from "aptos";
+import {FAUCET_URL, loadAdminFromConfig, TESTNET_URL, TestWallet} from "../src/utils";
+import {Staker} from "../src/staker";
+
+const main = async () => {
+    const aptosClient = new AptosClient(TESTNET_URL)
+    const faucetClient = new FaucetClient(TESTNET_URL, FAUCET_URL)
+    const admin = await loadAdminFromConfig()
+    const contractAddress = admin.toPrivateKeyObject().address as string
+    const wallet = new TestWallet(admin)
+    const staker = await Staker.build({ aptosClient, faucetClient, wallet, contractAddress })
+    const tx = await staker.addValidator()
+    console.log(tx)
+}
+
+main()
