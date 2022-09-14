e2b619afc444c4e2aade40888651ad09e8df27c0
Robert Godula <robertgodula1993@gmail.com>
2022-09-14 14:20:40 +0200

    add unit test

---
+++ sdk/scripts/unit-tests.ts
@@ -0,0 +1,9 @@
+import { AptosAccount } from 'aptos'
+import { loadAdminFromConfig, unitTests } from '../src/utils'
+
+const main = async () => {
+  const admin = await loadAdminFromConfig()
+  await unitTests(admin)
+}
+
+main()
