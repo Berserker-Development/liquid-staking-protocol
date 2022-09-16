80d6e35b075f31120922cede1deac05f6d4dadd4
Robert Godula <robertgodula1993@gmail.com>
2022-09-16 17:09:54 +0200

    init and pda fun

--- sdk/src/staker.ts
+++ sdk/src/staker.ts
@@ -1,4 +1,4 @@
-import { AptosClient, FaucetClient, MaybeHexString } from 'aptos'
+import { AptosClient, FaucetClient, HexString, MaybeHexString } from 'aptos'
 import {
   AccountAddress,
   ChainId,
@@ -8,23 +8,20 @@ import {
   TransactionPayloadEntryFunction
 } from 'aptos/dist/transaction_builder/aptos_types'
 
-import { IWallet } from './utils'
-import { ValidatorSet } from './interfaces'
+import { IWallet, StakerParams, StakerResource, ValidatorSet } from './interfaces'
 import { AptosCoin } from './types'
-import { bcsSerializeStr, bcsSerializeUint64 } from 'aptos/dist/transaction_builder/bcs'
+import { bcsSerializeBool, bcsSerializeUint64 } from 'aptos/dist/transaction_builder/bcs'
+import { Address } from 'aptos/dist/generated'
+import { sha3_256 } from 'js-sha3'
 
-export interface StakerParams {
-  aptosClient: AptosClient
-  faucetClient: FaucetClient
-  wallet: IWallet
-  contractAddress: string
-}
+const STAKER_SEED = 'Staker'
 
 export class Staker {
   private aptosClient: AptosClient
   private faucetClient: FaucetClient
   private wallet: IWallet
-  private contractAddress: string
+  private contractAddress: Address
+  private stakerResourceAddress: Address
 
   private constructor(params: StakerParams) {
     const { aptosClient, faucetClient, wallet, contractAddress } = params
@@ -32,10 +29,31 @@ export class Staker {
     this.faucetClient = faucetClient
     this.wallet = wallet
     this.contractAddress = contractAddress
+    this.stakerResourceAddress = this.getResourceAccountAddress(
+      new HexString(contractAddress),
+      STAKER_SEED
+    )
   }
 
   public static async build(params: StakerParams): Promise<Staker> {
     return new Staker(params)
+  }
+
+  private getResourceAccountAddress(address: HexString, seed: string) {
+    const toHex = require('to-hex')
+    const seedHex: string = toHex(seed)
+    const addressArray: Uint8Array = address.toUint8Array()
+    const seedArray: Uint8Array = Uint8Array.from(Buffer.from(seedHex, 'hex'))
+    return sha3_256(new Uint8Array([...addressArray, ...seedArray]))
+  }
+
+  public async init(monitorSupply: boolean, amount: number) {
+    const scriptFunctionPayload: TransactionPayloadEntryFunction = await this.initPayload(
+      monitorSupply,
+      amount
+    )
+    const rawTxn: RawTransaction = await this.getRawTransaction(scriptFunctionPayload)
+    return await this.signAndSend(rawTxn)
   }
 
   public async getRawTransaction(payload: TransactionPayload): Promise<RawTransaction> {
@@ -96,12 +114,41 @@ export class Staker {
     const balance: string = testCoinStore.data.coin.value
     return Number.parseInt(balance)
   }
-  public async getValidatorSet() {
+
+  public async getValidatorSet(): Promise<ValidatorSet> {
     return (await this.aptosClient.getAccountResource('0x1', `0x1::stake::ValidatorSet`))
       .data as ValidatorSet
   }
 
+  async getStakerResource(): Promise<StakerResource> {
+    const data = (
+      await this.aptosClient.getAccountResource(
+        this.stakerResourceAddress,
+        `${this.contractAddress}::core::Staker`
+      )
+    ).data as any
+
+    return {
+      fee: Number(data.fee),
+      stakerSignerCap: data.staker_signer_cap
+    }
+  }
+
   // PAYLOADS
+  public async initPayload(
+    monitorSupply: boolean,
+    fee: number
+  ): Promise<TransactionPayloadEntryFunction> {
+    return new TransactionPayloadEntryFunction(
+      EntryFunction.natural(
+        `${this.contractAddress}::core`,
+        'init',
+        [],
+        [bcsSerializeBool(monitorSupply), bcsSerializeUint64(fee)]
+      )
+    )
+  }
+
   public async stakePayload(newValue: number): Promise<TransactionPayloadEntryFunction> {
     return new TransactionPayloadEntryFunction(
       EntryFunction.natural(
