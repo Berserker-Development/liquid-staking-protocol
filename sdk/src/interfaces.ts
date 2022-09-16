e681ad1605ca1b0c6f3fca18af3b675b084437f1
Robert Godula <robertgodula1993@gmail.com>
2022-09-16 17:08:58 +0200

    update interfaces

--- sdk/src/interfaces.ts
+++ sdk/src/interfaces.ts
@@ -1,3 +1,22 @@
+import { AptosAccount, AptosClient, FaucetClient, HexString } from 'aptos'
+import { Address } from 'aptos/dist/generated'
+import { RawTransaction } from 'aptos/dist/transaction_builder/aptos_types'
+
+export interface StakerResource {
+  fee: number
+  stakerSignerCap: SignerCapability
+}
+
+export interface SignerCapability {
+  account: Address
+}
+export interface StakerParams {
+  aptosClient: AptosClient
+  faucetClient: FaucetClient
+  wallet: IWallet
+  contractAddress: Address
+}
+
 export interface ValidatorSet {
   active_validators: ActiveValidator[]
   consensus_scheme: number
@@ -18,4 +37,18 @@ export interface Config {
   fullnode_addresses: string
   network_addresses: string
   validator_index: string
+}
+
+export interface AptosConfig {
+  profiles: {
+    default: {
+      private_key: string
+    }
+  }
+}
+
+export interface IWallet {
+  signTransaction: (tx: RawTransaction) => Promise<Uint8Array>
+  signAllTransactions: (txs: RawTransaction[]) => Promise<Uint8Array[]>
+  account: AptosAccount
 }
