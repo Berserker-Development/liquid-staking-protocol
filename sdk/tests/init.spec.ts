52ff54f85b361bebea42da7771a5eca625245994
Robert Godula <robertgodula1993@gmail.com>
2022-09-16 17:10:18 +0200

    add init test

---
+++ sdk/tests/init.spec.ts
@@ -0,0 +1,59 @@
+import { AptosAccount, AptosClient, FaucetClient, HexString } from 'aptos'
+import {
+  compileAndDeploy,
+  FAUCET_URL,
+  getResourceAccountAddress,
+  sleep,
+  TESTNET_URL,
+  TestWallet
+} from '../src/utils'
+import { Staker } from '../src/staker'
+import assert from 'assert'
+import { StakerParams, StakerResource } from '../src/interfaces'
+import { Address } from 'aptos/dist/generated'
+
+describe('Init staker', () => {
+  let aptosClient: AptosClient
+  let faucetClient: FaucetClient
+  let staker: Staker
+  let ADMIN: AptosAccount
+  let contractAddress: Address
+
+  before(async () => {
+    aptosClient = new AptosClient(TESTNET_URL)
+    faucetClient = new FaucetClient(TESTNET_URL, FAUCET_URL)
+
+    ADMIN = new AptosAccount()
+
+    contractAddress = ADMIN.toPrivateKeyObject().address as Address
+    faucetClient.fundAccount(ADMIN.address(), 1_000_000_000)
+
+    await sleep(1000)
+    await compileAndDeploy(ADMIN)
+    await sleep(1000)
+  })
+
+  it('build instance', async () => {
+    const initParams: StakerParams = {
+      aptosClient,
+      faucetClient,
+      wallet: new TestWallet(ADMIN),
+      contractAddress
+    }
+
+    staker = await Staker.build(initParams)
+  })
+  it('init', async () => {
+    const stakerFee: number = 100
+    const hashIx = await staker.init(true, stakerFee)
+    console.log(hashIx)
+    const stakerResource: StakerResource = await staker.getStakerResource()
+
+    assert(stakerResource.fee === stakerFee, 'Invalid staker fee')
+    assert(
+      stakerResource.stakerSignerCap.account ===
+        '0x' + getResourceAccountAddress(new HexString(contractAddress), 'Staker'),
+      'Invalid pda address'
+    )
+  })
+})
