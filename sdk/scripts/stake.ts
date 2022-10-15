8718053d3a82a74b18cba1af125cc2fa47f4beec
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2022-10-16 01:07:42 +0200

    Update unstake script

--- sdk/scripts/stake.ts
+++ sdk/scripts/stake.ts
@@ -13,11 +13,11 @@ const main = async () => {
   const balanceBefore = await staker.getAptosCoinBalance(admin.address())
   console.log(`balance before = ${balanceBefore}`)
 
-  let tx = await staker.stake(100_000)
+  let tx = await staker.stake(1_000_000)
   console.log(tx)
 
   const balanceAfter = await staker.getAptosCoinBalance(admin.address())
-  console.log(`balance before = ${balanceAfter}`)
+  console.log(`balance after = ${balanceAfter}`)
 }
 
 main()
--- sdk/scripts/unstake.ts
+++ sdk/scripts/unstake.ts
@@ -3,21 +3,24 @@ import { loadAdminFromConfig, TESTNET_URL, FAUCET_URL, TestWallet } from '../src
 import { Staker } from '../src/staker'
 
 const main = async () => {
-    const aptosClient = new AptosClient(TESTNET_URL)
-    const faucetClient = new FaucetClient(TESTNET_URL, FAUCET_URL)
-    const admin = await loadAdminFromConfig()
-    const contractAddress = admin.toPrivateKeyObject().address as string
-    const wallet = new TestWallet(admin, aptosClient)
-    const staker = await Staker.build({ aptosClient, faucetClient, wallet, contractAddress })
+  const aptosClient = new AptosClient(TESTNET_URL)
+  const faucetClient = new FaucetClient(TESTNET_URL, FAUCET_URL)
+  const admin = await loadAdminFromConfig()
+  const contractAddress = admin.toPrivateKeyObject().address as string
+  const wallet = new TestWallet(admin, aptosClient)
+  const staker = await Staker.build({ aptosClient, faucetClient, wallet, contractAddress })
 
-    const balanceBefore = await staker.getAptosCoinBalance(admin.address())
-    console.log(`balance before = ${balanceBefore}`)
+  const balanceBefore = await staker.getAptosCoinBalance(admin.address())
+  console.log(`balance before = ${balanceBefore}`)
 
-    let tx = await staker.unstake(100_000)
-    console.log(tx)
+  let stakeTx = await staker.unstake(1_000_000)
+  console.log(`stake tx hash: ${stakeTx}`)
 
-    const balanceAfter = await staker.getAptosCoinBalance(admin.address())
-    console.log(`after before = ${balanceAfter}`)
+  const claimTx = await staker.claim()
+  console.log(`claim tx hash: ${claimTx}`)
+
+  const balanceAfter = await staker.getAptosCoinBalance(admin.address())
+  console.log(`balance after = ${balanceAfter}`)
 }
 
 main()
